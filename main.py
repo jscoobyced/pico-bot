38d95effd9ba0e0856b822f5db0870e8967a8350
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2023-08-04 22:09:17 +0700

    Add SC08A controller code and demo movement of the motors.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from oled import Oled
 from eyes import Eyes
+from head import Head
 import uasyncio
 
-async def play(display):
+async def play(display, motor):
     display.create_frames(Eyes.frames)
     print('Starting animation')
     await display.animate(Eyes.around, 3)
     await display.animate(Eyes.blink, 3)
     await display.animate(Eyes.around)
     await display.animate(Eyes.straight)
+    await motor.run()
     print('Done')
 
-eye = Oled(0, 128, 64, 16, 17)
+pico_eye = Oled(0, 128, 64, 16, 17)
+pico_head = Head()
 
-uasyncio.run(play(eye))
+uasyncio.run(play(pico_eye, pico_head))
 
